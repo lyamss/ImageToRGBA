267de34098e0a88324bf2b572dee4828dea8123b
Lyamss <161554647+lyamss@users.noreply.github.com>
2024-10-15 01:32:51 +0200

    Add code and ReleaseOS

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+Console.WriteLine("Please provide the image path as a command line argument :");
+string inputPath = Console.ReadLine();
+
+if (inputPath is null) { Console.WriteLine("Please provide the image path as a command line argument"); return; }
+
+if (!File.Exists(inputPath)) { Console.WriteLine("The specified file does not exist"); return; }
+
+string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "imageInRGBA.png");
+
+using (Bitmap originalBitmap = new(inputPath))
+{
+    Bitmap rgbaBitmap = new (originalBitmap.Width, originalBitmap.Height, PixelFormat.Format32bppArgb);
+
+    using (Graphics graphics = Graphics.FromImage(rgbaBitmap))
+    {
+        graphics.DrawImage(originalBitmap, new Rectangle(0, 0, originalBitmap.Width, originalBitmap.Height));
+    }
+    rgbaBitmap.Save(outputPath, ImageFormat.Png);
+}
+Console.WriteLine("The image has been converted to RGBA mode and saved successfully");
---
+++ README.md
@@ -0,0 +1,16 @@
+# ImageToRGBA Project in C#
+
+## Description
+
+ImageToRGBA is a C# project that allows you to convert images to PNG format with RGBA (Red, Green, Blue, Alpha) channels..
+
+## Features
+
+- Converting images to PNG format with RGBA channels.
+- Cross-platform support: Windows, macOS, Linux.
+
+## Prerequisites
+
+- .NET SDK installed on your machine.
+
+## You can find your OS version in the ReleaseOS folder
