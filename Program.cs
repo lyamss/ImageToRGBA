97f785e3664a01ad22118ac69af9654f6decfdf1
Lyamss <161554647+lyamss@users.noreply.github.com>
2024-10-16 11:18:59 +0200

    📌added supports jpg, jpeg, bmp, gif, ico, Webp, Heif 📌catch possibility error 📌Add icon

--- Program.cs
+++ Program.cs
@@ -1,22 +1,60 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
-Console.WriteLine("Please provide the image path as a command line argument :");
+Console.WriteLine("Please provide the image path as a command line argument:");
 string inputPath = Console.ReadLine();
 
-if (inputPath is null) { Console.WriteLine("Please provide the image path as a command line argument"); return; }
+if (inputPath is null) {Console.WriteLine("Please provide the image path as a command line argument"); return; }
 
 if (!File.Exists(inputPath)) { Console.WriteLine("The specified file does not exist"); return; }
 
-string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "imageInRGBA.png");
-
-using (Bitmap originalBitmap = new(inputPath))
-{
-    Bitmap rgbaBitmap = new (originalBitmap.Width, originalBitmap.Height, PixelFormat.Format32bppArgb);
-
-    using (Graphics graphics = Graphics.FromImage(rgbaBitmap))
+try
     {
-        graphics.DrawImage(originalBitmap, new Rectangle(0, 0, originalBitmap.Width, originalBitmap.Height));
+        string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path.GetFileNameWithoutExtension(inputPath) + "ImageInRGBA" + Path.GetExtension(inputPath));
+
+        using (Bitmap originalBitmap = new(inputPath))
+        {
+            Bitmap rgbaBitmap = new(originalBitmap.Width, originalBitmap.Height, PixelFormat.Format32bppArgb);
+
+            using (Graphics graphics = Graphics.FromImage(rgbaBitmap))
+            {
+                graphics.DrawImage(originalBitmap, new Rectangle(0, 0, originalBitmap.Width, originalBitmap.Height));
+            }
+
+            ImageFormat format;
+            switch (Path.GetExtension(inputPath).ToLower())
+            {
+                case ".jpg":
+                case ".jpeg":
+                    format = ImageFormat.Jpeg;
+                    break;
+                case ".bmp":
+                    format = ImageFormat.Bmp;
+                    break;
+                case ".gif":
+                    format = ImageFormat.Gif;
+                    break;
+                case ".png":
+                    format = ImageFormat.Png;
+                    break;
+                case ".ico":
+                    format = ImageFormat.Icon;
+                    break;
+                case ".Webp":
+                    format = ImageFormat.Webp;
+                    break;
+                case ".Heif":
+                    format = ImageFormat.Heif;
+                    break;
+            default:
+                    Console.WriteLine("Unsupported image format");
+                    return;
+            }
+
+            rgbaBitmap.Save(outputPath, format);
+        }
+        Console.WriteLine("The image has been converted to RGBA mode and saved successfully :)");
     }
-    rgbaBitmap.Save(outputPath, ImageFormat.Png);
-}
-Console.WriteLine("The image has been converted to RGBA mode and saved successfully");
+    catch (ArgumentException e)
+    { Console.WriteLine("The image appears corrupted (Try with other image :/) => " + e.Message); return; }
+    catch (Exception e)
+    { Console.WriteLine("Other error, please reports the bug => " + e.Message); return; }
--- README.md
+++ README.md
@@ -4,13 +4,31 @@
 
 ImageToRGBA is a C# project that allows you to convert images to PNG format with RGBA (Red, Green, Blue, Alpha) channels..
 
+## Supports
+
+```
+jpg
+jpeg
+bmp
+gif
+png
+ico
+Webp
+Heif
+```
+
 ## Features
 
 - Converting images to PNG format with RGBA channels.
 - Cross-platform support: Windows, macOS, Linux.
 
+<p align="center">
+  <img src="./exemple.png" width="600" height="400" alt="ExempleInWindows">
+</p>
+
 ## Prerequisites
 
 - .NET SDK installed on your machine.
 
-## You can find your OS version in the ReleaseOS folder
+## Download
+You can find your OS version in the ReleaseOS folder
